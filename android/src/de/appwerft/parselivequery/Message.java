9c78b06608cc10fe580021aecd31ab599cc0d074
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-04-20 16:39:46 +0200

           

--- android/src/de/appwerft/parselivequery/Message.java
+++ android/src/de/appwerft/parselivequery/Message.java
@@ -1,46 +1,29 @@
 package de.appwerft.parselivequery;
 
-import java.util.Date;
-
-import com.parse.ParseObject;
 import com.parse.ParseClassName;
-import com.parse.ParseUser;
+import com.parse.ParseObject;
 
 @ParseClassName("Message")
 public class Message extends ParseObject {
+	public static final String USER_ID_KEY = "userId";
+	public static final String BODY_KEY = "body";
 
-	public Message() {
+	public String getUserId() {
+		return getString(USER_ID_KEY);
 	}
 
-	public String getId() {
-		return getObjectId();
+	public String getBody() {
+
+		return getString(BODY_KEY);
 	}
 
-	public Date getDate() {
-		return getCreatedAt();
+	public void setUserId(String userId) {
+
+		put(USER_ID_KEY, userId);
 	}
 
-	public ParseUser getSender() {
-		return getParseUser("sender");
-	}
+	public void setBody(String body) {
+		put(BODY_KEY, body);
 
-	public void setSender(ParseUser user) {
-		put("sender", user);
-	}
-
-	public ParseUser getReceiver() {
-		return getParseUser("receiver");
-	}
-
-	public void setReceiver(ParseUser user) {
-		put("receiver", user);
-	}
-
-	public String getContent() {
-		return getString("content");
-	}
-
-	public void setContent(String content) {
-		put("content", content);
 	}
 }
--- android/src/de/appwerft/parselivequery/QueryProxy.java
+++ android/src/de/appwerft/parselivequery/QueryProxy.java
@@ -1,11 +1,16 @@
 package de.appwerft.parselivequery;
 
 import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.kroll.common.Log;
 
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SubscriptionHandling;
 
 // This proxy can be created by calling Parselivequery.createExample({message: "hello world"})
@@ -13,7 +18,10 @@ import com.parse.SubscriptionHandling;
 public class QueryProxy extends KrollProxy {
 	// Standard Debugging variables
 	private static final String LCAT = "PLQ";
-	private ParseQuery<Message> query;
+	private ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+	static final String USER_ID_KEY = "userId";
+
+	static final String BODY_KEY = "body";
 
 	// Constructor
 	public QueryProxy() {
@@ -21,8 +29,29 @@ public class QueryProxy extends KrollProxy {
 	}
 
 	@Override
-	public void handleCreationDict(KrollDict options) {
-		super.handleCreationDict(options);
-		query = ParseQuery.getQuery(Message.class);
+	public void handleCreationDict(KrollDict opts) {
+
+		super.handleCreationDict(opts);
+	}
+
+	@Kroll.method
+	public void post(KrollDict opts) {
+		KrollDict data = opts.getKrollDict("data");
+		KrollFunction onSuccess = (KrollFunction) opts.get("onsuccess");
+		KrollFunction onError = (KrollFunction) opts.get("onerror");
+
+		ParseObject message = ParseObject.create("Message");
+		message.put(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+		message.put(BODY_KEY, data);
+		message.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e == null) {
+					onSuccess.call(getKrollObject(), new KrollDict());
+				} else {
+					onError.call(getKrollObject(), new KrollDict());
+				}
+			}
+		});
 	}
 }
