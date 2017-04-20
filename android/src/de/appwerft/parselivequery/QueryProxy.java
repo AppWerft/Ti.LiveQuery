c138268c7cdbffcd72080362df9c50114790a467
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-04-20 17:01:40 +0200

          

--- android/src/de/appwerft/parselivequery/QueryProxy.java
+++ android/src/de/appwerft/parselivequery/QueryProxy.java
@@ -1,11 +1,14 @@
 package de.appwerft.parselivequery;
 
+import java.util.List;
+
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.kroll.common.Log;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -22,6 +25,7 @@ public class QueryProxy extends KrollProxy {
 	static final String USER_ID_KEY = "userId";
 
 	static final String BODY_KEY = "body";
+	static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
 
 	// Constructor
 	public QueryProxy() {
@@ -53,5 +57,34 @@ public class QueryProxy extends KrollProxy {
 				}
 			}
 		});
+	}
+
+	@Kroll.method
+	void refreshMessages(KrollDict opts) {
+		KrollFunction onSuccess = (KrollFunction) opts.get("onsuccess");
+		KrollFunction onError = (KrollFunction) opts.get("onerror");
+		ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+		query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+		// get the latest 50 messages, order will show up newest to oldest of
+		// this group
+		query.orderByDescending("createdAt");
+		// Execute query to fetch all messages from Parse asynchronously
+		// This is equivalent to a SELECT query with SQL
+		query.findInBackground(new FindCallback<Message>() {
+			public void done(List<Message> messages, ParseException e) {
+				if (e == null) {
+					KrollDict res = new KrollDict();
+					res.put("messages", messages.toArray());
+					onSuccess.call(getKrollObject(), res);
+				} else {
+
+					Log.e("message", "Error Loading Messages" + e);
+
+				}
+
+			}
+
+		});
+
 	}
 }
